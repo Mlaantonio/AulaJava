c9332aef97806ceba32d6dd4076bf72ab6484a45
Mario Antonio <90258182+Mlaantonio@users.noreply.github.com>
2023-08-15 19:42:46 -0300

    Aula de Java
    Inicio da aula de Java

---
+++ Principal.java
@@ -0,0 +1,8 @@
+public class Principal {
+    public static void main(String[] args) {  //String[] = 
+        Livro java;
+        java.codigo = 1234;
+        java.nome = "Java: Como Programar";
+        
+    }
+}
